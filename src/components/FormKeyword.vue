7c06d805172d1f293f308bb6f54509adb93d7403
bell <weerin.n@ku.th>
2021-06-18 17:34:38 +0700

    keyword page &  component

---
+++ src/components/FormKeyword.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="form-keyword">
+    <div class="container">
+      <h1>Create Keyword</h1>
+      <form>
+        <div class="form-row">
+          <div class="col-6">
+            <input type="text" class="form-control" placeholder="Thai-Word" />
+          </div>
+          <div class="col-6">
+            <input type="text" class="form-control" placeholder="Eng-Word" />
+          </div>
+        </div>
+        <button v-on="addKeyword()" type="submit" class="btn btn-primary">Create</button>
+        <button type="reset" value="Reset" class="btn btn-warning">
+          Clear
+        </button>
+      </form>
+
+      <br />
+    </div>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+    name: 'FormKeyword',
+    data() {
+        return { 
+            Keyword: {
+                word : ''
+            }
+        }
+    },
+    methods: {
+        addKeyword(){
+            axios.post()
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/TableKeyword.vue
+++ src/components/TableKeyword.vue
@@ -1,47 +1,47 @@
 <template>
-    <div id="table-keyword">
-        <div class="container">
-      <table class="table" id="job-table">
-        <thead>
-          <tr class="table-warning">
-            <th scope="col">Keyword</th>
-            <th scope="col">Service</th>
-            <th scope="col">Page</th>
-            <th scope="col">Status</th>
-            <th scope="col">Start</th>
-            <th scope="col">End</th>
-          </tr>
-        </thead>
-        <tbody>
-          <!-- <tr v-for="job in Jobs" :key="job.id">
-            <td>{{job.keyword}}</td>
-            <td>{{job.service}}</td>
-            <td>{{job.page}}</td>
-            <td>{{job.status}}</td>
-            <td>{{job.start_time}}</td>
-            <td>{{job.end_time}}</td>
-          </tr> -->
-         </tbody>
-      </table>
-    </div>
-
-    </div>
-    
+  <div id="table-keyword">
+    <h1>All Keyword Table</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">id</th>
+          <th scope="col">word(thai/eng)</th>
+          <th scope="col">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="keyword in Keywords" :key="keyword">
+          <th scope="row">1</th>
+          <td>{{keyword.id}}</td>
+          <td>{{keyword.word}}</td>
+          <td>
+            <button class="btn btn-danger">Delete</button>
+            <!-- <button class="btn btn-warning">edit</button> -->
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-    name: "TableKeyword",
-    mounted(){
-        
-    },
-    methods: {
-        
+  name: "TableKeyword",
+  data(){
+    return {
+      Keywords: {}
     }
-
-}
+  },
+  mounted() {
+    axios.get("http://localhost:3000/keyword").then((response) => {
+      this.Keywords = response.data.keywords;
+    })
+  },
+  methods: {},
+};
 </script>
 
 <style scoped>
-
 </style>
